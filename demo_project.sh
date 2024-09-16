8e5e33c4c10c688c5568f28c6a4470ba1c812a87
seifsherif1544 <seifsherif1544@gmail.com>
2024-09-16 19:45:24 +0300

    adding new feautures

--- demo_project.sh
+++ demo_project.sh
@@ -1,5 +1,42 @@
+#! /bin/bash
+
+
+
+echo "------------------------- Showing images & Containers -------------------------------"
+
+docker images
+
+docker ps
+
+
+
+echo "------------------------- Deleting images & Containers -------------------------------"
+
+docker stop $(docker ps -a -q)
+
+
+docker rm $(docker ps -a -q)
+
+docker rmi -f $(docker images -aq)  
+
+#docker rm -vf $(docker ps -aq) 
+
+echo "------------------------- Showing images & Containers -------------------------------"
+
+docker images
+
+docker ps
+
+echo "--------------------------- Bulding image from Dockerfile -----------------------------"
+
 docker build -t my-nginx-static-site .
 
-docker run -d -p 8080:80 my-nginx-static-site
+echo "----------------------------- Create Container from image -----------------------------"
 
-curl http://localhost:8080
+docker run -d -p 8081:80 my-nginx-static-site
+
+echo "---------------------------------- Showing Web-Site -----------------------------------"
+
+sleep 20s
+
+curl localhost:8081
